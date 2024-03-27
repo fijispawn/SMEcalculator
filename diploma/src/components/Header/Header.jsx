8a5b0900fb45926de4ecfdef3b1f418a14382642
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-03-27 13:20:17 +0300

    imported hooks

--- diploma/src/components/Header/Header.jsx
+++ diploma/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import HeaderIn from "./HeaderIn";
 import HeaderOut from "./HeaderOut";
 import { useAuth } from "../../hooks/AuthContext";

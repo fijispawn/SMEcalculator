b64e2d63b2bf91d702f2c389d96e8a0b066c8deb
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-05-03 13:39:31 +0300

    same as previous

--- diploma/src/hooks/AuthContext.jsx
+++ diploma/src/hooks/AuthContext.jsx
@@ -1,3 +1,9 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setLoggedIn] = useState(() => {
     const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
@@ -15,3 +21,6 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+
+

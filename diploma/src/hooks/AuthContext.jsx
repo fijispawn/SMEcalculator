386494c0aeaf882467917e6d1c169bed4336532e
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-05-03 13:29:22 +0300

    trying to fix AuthContext hook

--- diploma/src/hooks/AuthContext.jsx
+++ diploma/src/hooks/AuthContext.jsx
@@ -1,14 +1,16 @@
-import React, { createContext, useContext, useState } from 'react';
-
-const AuthContext = createContext();
-
-export const useAuth = () => useContext(AuthContext);
-
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(() => {
+    const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
+    return storedIsLoggedIn === 'true';
+  });
+
+  const handleLogin = (status) => {
+    setLoggedIn(status);
+    localStorage.setItem('isLoggedIn', status); 
+  };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn: handleLogin }}>
       {children}
     </AuthContext.Provider>
   );

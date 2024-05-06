bd80366ff99626e24e8589266a86e2b0afbad791
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-05-06 16:47:30 +0300

    updated filledgraphs

--- diploma/src/components/Analytics/FilledGraphs/FilledGraphs.jsx
+++ diploma/src/components/Analytics/FilledGraphs/FilledGraphs.jsx
@@ -1,53 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import styles from "./FilledGraphs.module.css";
-// import { MdEdit } from "react-icons/md";
-// import { FaTrashAlt } from "react-icons/fa";
-// import Empty from "./Empty";
-
-// const FilledGraphs = ({ setHasData }) => {
-//   const [dates, setDates] = useState({});
-
-//   useEffect(() => {
-//     fetch("https://enterpizemate.dyzoon.dev/api/analytics/get-costs")
-//       .then(response => {
-//         if (response.ok) {
-//           return response.json();
-//         }
-//         throw new Error("Network response was not ok.");
-//       })
-//       .then(data => {
-//         console.log("Received data:", data);
-//         setDates(data);
-//         setHasData(Object.keys(data).length > 0);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//         setDates({});
-//         setHasData(false);
-//       });
-//   }, [setHasData]);
-
-//   if (Object.keys(dates).length === 0) {
-//     return <Empty />;
-//   }
-
-//   return (
-//     <div className={styles.wrapper}>
-//       {Object.entries(dates).map(([date, details], index) => (
-//         <div key={index} className={styles.container}>
-//           <span className="text-left">{date}</span>
-//           <div className="flex justify-end items-center">
-//             <MdEdit /> Изменить
-//             <FaTrashAlt />
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default FilledGraphs;
-
 import React, { useEffect, useState } from "react";
 import styles from "./FilledGraphs.module.css";
 import { MdEdit } from "react-icons/md";
@@ -55,47 +5,97 @@ import { FaTrashAlt } from "react-icons/fa";
 import Empty from "./Empty";
 
 const FilledGraphs = ({ setHasData }) => {
-    const [dates, setDates] = useState({});
+  const [dates, setDates] = useState({});
 
-    useEffect(() => {
-      const mockData = {
-        '2024-01-01': { summ: 500 },
-        '2024-02-01': { summ: 300 },
-        '2024-03-01': { summ: 450 },
-        '2024-04-01': { summ: 700 },
-        '2024-08-01': { summ: 700 },
-        '2025-01-01': { summ: 150 },
-        '2025-02-01': { summ: 250 },
-        '2025-03-01': { summ: 350 },
-        '2026-01-01': { summ: 120 },
-        '2026-02-01': { summ: 180 },
-        '2026-03-01': { summ: 180 },
-        '2026-04-01': { summ: 180 }
-      };
-        setTimeout(() => {
-            setDates(mockData);
-            setHasData(Object.keys(mockData).length > 0);
-        }, 1000);  
-    }, [setHasData]);
+  useEffect(() => {
+    fetch("https://enterpizemate.dyzoon.dev/api/analytics/get-costs")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(data => {
+        console.log("Received data:", data);
+        setDates(data);
+        setHasData(Object.keys(data).length > 0);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setDates({});
+        setHasData(false);
+      });
+  }, [setHasData]);
 
-    if (Object.keys(dates).length === 0) {
-        return <Empty />;
-    }
+  if (Object.keys(dates).length === 0) {
+    return <Empty />;
+  }
 
-    return (
-        <div className={styles.wrapper}>
-            {Object.entries(dates).map(([date, details], index) => (
-                <div key={index} className={styles.container}>
-                    <span className="text-left">{date}</span>
-                    <div className="flex justify-end items-center">
-                        <MdEdit /> Изменить
-                        <FaTrashAlt />
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className={styles.wrapper}>
+      {Object.entries(dates).map(([date, details], index) => (
+        <div key={index} className={styles.container}>
+          <span className="text-left">{date}</span>
+          <div className="flex justify-end items-center">
+            <MdEdit /> Изменить
+            <FaTrashAlt />
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default FilledGraphs;
+
+// import React, { useEffect, useState } from "react";
+// import styles from "./FilledGraphs.module.css";
+// import { MdEdit } from "react-icons/md";
+// import { FaTrashAlt } from "react-icons/fa";
+// import Empty from "./Empty";
+
+// const FilledGraphs = ({ setHasData }) => {
+//     const [dates, setDates] = useState({});
+
+//     useEffect(() => {
+//       const mockData = {
+//         '2024-01-01': { summ: 500 },
+//         '2024-02-01': { summ: 300 },
+//         '2024-03-01': { summ: 450 },
+//         '2024-04-01': { summ: 700 },
+//         '2024-08-01': { summ: 700 },
+//         '2025-01-01': { summ: 150 },
+//         '2025-02-01': { summ: 250 },
+//         '2025-03-01': { summ: 350 },
+//         '2026-01-01': { summ: 120 },
+//         '2026-02-01': { summ: 180 },
+//         '2026-03-01': { summ: 180 },
+//         '2026-04-01': { summ: 180 }
+//       };
+//         setTimeout(() => {
+//             setDates(mockData);
+//             setHasData(Object.keys(mockData).length > 0);
+//         }, 1000);  
+//     }, [setHasData]);
+
+//     if (Object.keys(dates).length === 0) {
+//         return <Empty />;
+//     }
+
+//     return (
+//         <div className={styles.wrapper}>
+//             {Object.entries(dates).map(([date, details], index) => (
+//                 <div key={index} className={styles.container}>
+//                     <span className="text-left">{date}</span>
+//                     <div className="flex justify-end items-center">
+//                         <MdEdit /> Изменить
+//                         <FaTrashAlt />
+//                     </div>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// };
+
+// export default FilledGraphs;
 

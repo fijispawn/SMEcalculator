9fa4596610670b808a4b2932b35c758ef1b3cc8b
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-05-07 16:23:46 +0300

    changed modal logic

--- diploma/src/components/Modal/SelectYearModal.jsx
+++ diploma/src/components/Modal/SelectYearModal.jsx
@@ -2,44 +2,38 @@ import React, { useState } from "react";
 import styles from "./SelectYearModal.module.css";
 import Button from "../Button/Button";
 
-
 const SelectYearModal = ({ isOpen, onClose, onSelectYear }) => {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-  const years = Array.from(
-    { length: 10 },
-    (_, index) => new Date().getFullYear() + index
-  );
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const years = Array.from({ length: 10 }, (_, index) => new Date().getFullYear() - index);
 
   if (!isOpen) {
     return null;
   }
 
-  const handleContentClick = (e) => {
-    e.stopPropagation();
+  const handleContentClick = (e) => e.stopPropagation();
+
+  const handleYearChange = (e) => {
+    setSelectedYear(parseInt(e.target.value));
+    setIsButtonDisabled(false); 
+  };
+
+  const handleClick = () => {
+    onSelectYear(selectedYear);
+    onClose(); 
   };
 
   return (
-    <div
-      className={`${styles.modal} ${isOpen ? styles.active : ""}`}
-      onClick={onClose}
-    >
+    <div className={`${styles.modal} ${isOpen ? styles.active : ""}`} onClick={onClose}>
       <div className={styles.content} onClick={handleContentClick}>
-        <h2 className="w-[200px]">
-          Выберите за какой год показать график 
-        </h2>
-        <select
-          value={selectedYear}
-          onChange={(e) => setSelectedYear(e.target.value)}
-          className={styles.select}
-        >
+        <h2>Выберите за какой год показать график</h2>
+        <select value={selectedYear} onChange={handleYearChange} className={styles.select}>
           {years.map((year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
+            <option key={year} value={year}>{year}</option>
           ))}
         </select>
         <div className={styles.buttons}>
-          <button className="w-[200px]" onClick={() => onSelectYear(selectedYear)}>Выбрать</button>
+          <Button text={`График за ${selectedYear}`} disabled={isButtonDisabled} onClick={handleClick}/>
         </div>
       </div>
     </div>

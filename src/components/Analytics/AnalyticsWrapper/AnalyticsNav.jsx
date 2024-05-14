1e43f25c59c112c5ccfc989198d60526b0222651
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-05-14 13:22:14 +0300

    bug

--- src/components/Analytics/AnalyticsWrapper/AnalyticsNav.jsx
+++ src/components/Analytics/AnalyticsWrapper/AnalyticsNav.jsx
@@ -11,7 +11,7 @@ const AnalyticsTabs = ({ activeTab }) => {
       <Link to="/overhead-analytics" className={`tab ${activeTab === 'overhead-analytics' ? 'tab__main' : 'tab'}`}>
         Накладные расходы
       </Link>
-      <Link to="/budget-analytics" className={`tab ${activeTab === 'budget-analytics' ? 'tab__main' : ''}`}>
+      <Link to="/budget-analytics" className={`tab ${activeTab === 'budget-analytics' ? 'tab__main' : 'tab'}`}>
         Бюджет
       </Link>
       <Link to="/cashflow-analytics" className={`tab ${activeTab === 'cashflow-analytics' ? 'tab__main' : ''}`}>

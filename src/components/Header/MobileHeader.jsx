f13fc447f3edbff7be30b9dcb5870e2d48d29532
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-05-15 12:58:58 +0300

    fixed mobile nav

--- src/components/Header/MobileHeader.jsx
+++ src/components/Header/MobileHeader.jsx
@@ -10,27 +10,74 @@ const MobileHeader = () => {
   const location = useLocation();
 
   const isActive = (pathArray) => {
-    return pathArray.some(path => location.pathname === path || location.pathname.startsWith(path + '/'));
+    return pathArray.some(
+      (path) =>
+        location.pathname === path || location.pathname.startsWith(path + "/")
+    );
   };
 
   return (
     <div>
       <div className="mobile__header">
         <Link to="/balance">
-          <div className={`flex flex-col items-center w-[80px] ${isActive(['/balance', '/overhead', '/budget', '/cashflow']) ? 'active' : ''}`}>
-            <FiActivity color={isActive(['/balance', '/overhead', '/budget', '/cashflow']) ? '#fd7770' : 'inherit'} />
+          <div
+            className={`flex flex-col items-center w-[80px] ${
+              isActive(["/balance", "/overhead", "/budget", "/cashflow"])
+                ? "active"
+                : ""
+            }`}
+          >
+            <FiActivity
+              color={
+                isActive(["/balance", "/overhead", "/budget", "/cashflow"])
+                  ? "#fd7770"
+                  : "inherit"
+              }
+            />
             Показатели
           </div>
         </Link>
         <Link to="/balance-analytics">
-          <div className={`flex flex-col items-center w-[80px] ${isActive(['/balance-analytics', '/cashflow-analytics', '/overhead-analytics']) ? 'active' : ''}`}>
-            <SiGoogleanalytics color={isActive(['/balance-analytics', '/cashflow-analytics', '/overhead-analytics']) ? '#fd7770' : 'inherit'} />
+          <div
+            className={`flex flex-col items-center w-[80px] ${
+              isActive([
+                "/balance-analytics",
+                "/cashflow-analytics",
+                "/budget-analytics",
+                "/overhead-analytics",
+              ])
+                ? "active"
+                : ""
+            }`}
+          >
+            <SiGoogleanalytics
+              color={
+                isActive([
+                  "/balance-analytics",
+                  "/cashflow-analytics",
+                  "/budget-analytics",
+                  "/overhead-analytics",
+                ])
+                  ? "#fd7770"
+                  : "inherit"
+              }
+            />
             Аналитика
           </div>
         </Link>
         <Link to="/account">
-          <div className={`flex flex-col items-center w-[80px] ${isActive(['/account', '/security', '/license']) ? 'active' : ''}`}>
-            <MdManageAccounts color={isActive(['/account', '/security', '/license']) ? '#fd7770' : 'inherit'} />
+          <div
+            className={`flex flex-col items-center w-[80px] ${
+              isActive(["/account", "/security", "/license"]) ? "active" : ""
+            }`}
+          >
+            <MdManageAccounts
+              color={
+                isActive(["/account", "/security", "/license"])
+                  ? "#fd7770"
+                  : "inherit"
+              }
+            />
             Аккаунт
           </div>
         </Link>
